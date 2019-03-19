40487b0fd6d270a1d3f3136b79d872879df3fc0e
Sten <StenKarlsson@users.noreply.github.com>
2019-03-19 15:47:48 +0100

    Revert "Merge pull request #1 from mixedbystefan/Stefan"
    This reverts commit 74ef2db96184bdffa53f3555fa82e21ae92a53fe, reversing
    changes made to f787294c53c8290f9c5ca238394aa1477fcba2e8.

--- src/main/java/museumHeist/sprites/GameCharacter.java
+++ src/main/java/museumHeist/sprites/GameCharacter.java
@@ -2,29 +2,17 @@ package museumHeist.sprites;
 
 import java.awt.geom.Point2D;
 
-import javax.imageio.ImageTypeSpecifier;
-
 import museum_heist.GameBoard;
 import museum_heist.Position;
 
 public class GameCharacter extends GameItem{
 
-	Position currentPosition;
-	int life = 3;
+	Position currentPosition; 
 	
 	public GameCharacter() {
-		int startx = 2;
-		int starty = 2; 
-		
+		int startx = 0;
+		int starty = 0; 
 		currentPosition = new Position(startx,starty); 
-	}
-	
-	public int getLife() {
-		return life;
-	}
-
-	public void setLife(int life) {
-		this.life = life;
 	}
 	
 	//startposition kan sättas via konstruktorn
@@ -32,13 +20,12 @@ public class GameCharacter extends GameItem{
 		setCurrentPosition(x,y); 
 	}
 	
-	public int getGameBoardValueOfCurrentPosition(int level) {
+	public int getGameBoardValueOfCurrentPosition() {
 		GameBoard gb = new GameBoard(); 
-		return gb.getGameBoardValue(currentPosition, level);
+		return gb.getGameBoardValue(currentPosition);
 	}
 
-	public void setCurrentPosition(int x, int y) 
-	{
+	public void setCurrentPosition(int x, int y) {
 		this.currentPosition = new Position(x,y); 
 	}
 
@@ -55,62 +42,7 @@ public class GameCharacter extends GameItem{
 		setCurrentPosition((int)currentPosition.getX()+1,(int)(currentPosition.getY()));  
 		}
 	
-	// Hämtar aktuell position
 	public Position getCurrentPosition() {
 		return currentPosition;
 	}
-
-	
-	public boolean isCollidingWith(Wall wall) {
-		
-		return currentPosition.equals(wall.getPosition());
-	}
-
-	public boolean grabsTreasure(Treasure treasure) {
-		
-		return currentPosition.equals(treasure.getPosition());
-	
-	}
-	
-	public boolean isCollidingWith(Laser laser) {
-		
-		
-		return currentPosition.equals(laser.getPosition());
-		
-	}
-	
-	// Karaktären dör om den krockar med en laser och har 0 liv kvar
-
-	public boolean isDead() 
-	{
-	
-		return this.life==0;
-	}
-	
-	// Om karaktären dör så tappar denna ett liv och flyttas till startpositionen
-	public void isKilled() 
-	{
-		this.life--;
-		setCurrentPosition(2, 2);
-		
-	}
-
-	
-
-	public void updatePosition(GameBoard gameBoard, int level) 
-	{
-		// Om spelplanens position ger värdet 5 har karaktären gått in i en laser
-		if (5==gameBoard.getGameBoardValue(this.currentPosition, level))
-		{
-			this.isKilled();
-		}
-		
-		
-	}
-
-	
-
-	
-
-	
 	}
--- src/test/java/characterMovementTests/registerPositionOnGamBoard.java
+++ src/test/java/characterMovementTests/registerPositionOnGamBoard.java
@@ -2,74 +2,60 @@ package characterMovementTests;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import museumHeist.sprites.GameCharacter;
-import museum_heist.GameBoard;
 import museum_heist.Position;
 
 public class registerPositionOnGamBoard {
-	GameCharacter mainCharacter;
-	GameBoard gameBoard;
-	// Körs innan varje test och eftersom startpositionen är densamma kan den sättas här
-	@Before
-	public void setup()
-	{
-		mainCharacter = new GameCharacter(0,0);
-		gameBoard = new GameBoard();
-		
-		
-	}
-
 	//Här testas ifall spelkaraktären kan känna av vilket värde som finns
 	//på den plats som den är på i 2darrayen som är spelbrädet
 	
-	//Om figuren inte har flyttat sig ska den läsa in värde 0
+	//Om figuren inte har flyttat sig ska den läsa in värde 1
 	@Test
 	public void dontMoveGetValue() {
-		System.out.println(mainCharacter.getGameBoardValueOfCurrentPosition(0));
-		assertEquals(1,mainCharacter.getGameBoardValueOfCurrentPosition(0)); 
+		GameCharacter mainCharacter = new GameCharacter(); //har 0,0 som startposition
+		
+		System.out.println(mainCharacter.getGameBoardValueOfCurrentPosition());
+		assertEquals(1,mainCharacter.getGameBoardValueOfCurrentPosition()); 
 	}
 	
 	//Om figuren flyttas 1 steg åt höger ska den läsa in värde 2
 		@Test
 		public void moveRightGetValue() {
+			GameCharacter mainCharacter = new GameCharacter(); //har 0,0 som startposition
 			mainCharacter.moveOneTileRight();
-			System.out.println(mainCharacter.getGameBoardValueOfCurrentPosition(0));
-			assertEquals(2,mainCharacter.getGameBoardValueOfCurrentPosition(0));  
+			System.out.println(mainCharacter.getGameBoardValueOfCurrentPosition());
+			assertEquals(2,mainCharacter.getGameBoardValueOfCurrentPosition()); 
 		}
 		//Om figuren flyttas 1 steg åt höger och sedan ett steg åt vänster ska den läsa in värde 1
-		@Test
-		public void moveRightLeftGetValue() {
-					
-			mainCharacter.moveOneTileRight();
-			mainCharacter.moveOneTileLeft();
-			System.out.println(mainCharacter.getGameBoardValueOfCurrentPosition(0));
-			assertEquals(1,mainCharacter.getGameBoardValueOfCurrentPosition(0));  
-		}
-		//Om figuren flyttas 1 steg åt höger och 1 steg ned ska den läsa in värde 7
-		@Test
-		public void moveRightDownGetValue() {
-					 
-			mainCharacter.moveOneTileRight();
-			mainCharacter.moveOneTileDown();
-			System.out.println(mainCharacter.getGameBoardValueOfCurrentPosition(0));
-			assertEquals(7,mainCharacter.getGameBoardValueOfCurrentPosition(0)); 
-		}
+				@Test
+				public void moveRightLeftGetValue() {
+					GameCharacter mainCharacter = new GameCharacter(); //har 0,0 som startposition
+					mainCharacter.moveOneTileRight();
+					mainCharacter.moveOneTileLeft();
+					System.out.println(mainCharacter.getGameBoardValueOfCurrentPosition());
+					assertEquals(1,mainCharacter.getGameBoardValueOfCurrentPosition()); 
+				}
+				//Om figuren flyttas 1 steg åt höger och 1 steg ned ska den läsa in värde 7
+				@Test
+				public void moveRightDownGetValue() {
+					GameCharacter mainCharacter = new GameCharacter(); //har 0,0 som startposition
+					mainCharacter.moveOneTileRight();
+					mainCharacter.moveOneTileDown();
+					System.out.println(mainCharacter.getGameBoardValueOfCurrentPosition());
+					assertEquals(7,mainCharacter.getGameBoardValueOfCurrentPosition()); 
+				}
 				
-		//Om figuren flyttas 1 steg åt höger och 1 steg ned sedan 2 steg upp ska den läsa in värde 5
-		@Test
-		public void moveRightDownUpGetValue() {
-					
-			mainCharacter.moveOneTileRight();
-			mainCharacter.moveOneTileDown();
-			mainCharacter.moveOneTileUp();
-			System.out.println(mainCharacter.getGameBoardValueOfCurrentPosition(0));
-			assertEquals(2,mainCharacter.getGameBoardValueOfCurrentPosition(0));  
-		}
-		
-		
-		
+				//Om figuren flyttas 1 steg åt höger och 1 steg ned sedan 1 steg upp ska den läsa in värde 2
+				@Test
+				public void moveRightDownUpGetValue() {
+					GameCharacter mainCharacter = new GameCharacter(); //har 0,0 som startposition
+					mainCharacter.moveOneTileRight();
+					mainCharacter.moveOneTileDown();
+					mainCharacter.moveOneTileUp();
+					System.out.println(mainCharacter.getGameBoardValueOfCurrentPosition());
+					assertEquals(2,mainCharacter.getGameBoardValueOfCurrentPosition()); 
+				}
 	
 }
