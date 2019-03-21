7a72c504b935327abf525119c061ff317211f705
Stefan Sundström <mixedbystefan@gmail.com>
2019-03-21 15:33:21 +0100

    Tråd i Main och setOpaque så grafiken syns..på mac

--- src/main/java/museumHeist/sprites/Main.java
+++ src/main/java/museumHeist/sprites/Main.java
@@ -1,17 +1,38 @@
 package museumHeist.sprites;
 
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import museumHeist.GUI.Classes.GameBoardInterface;
+
+import museum_heist.GameBoard;
+import museum_heist.Position;
+
 public class Main {
 	//skapade en mainklass bara för att leka med objekten lite 
-	public static void main(String[] args) {
-		GameCharacter c1 = new GameCharacter(); 
-		GameCharacter c2 = new GameCharacter(0,1); 
-		System.out.println(c1.getCurrentPosition());
-		
-		c1.moveOneTileRight();
-		System.out.println(c1.getCurrentPosition());
-		
-		
-		
-	}
 	
-}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameBoardInterface gb = new GameBoardInterface();
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}}
+
