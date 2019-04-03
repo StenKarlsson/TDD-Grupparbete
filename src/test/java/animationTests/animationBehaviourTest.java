97bd25f2003ae44b65bfb967425ab5e72858904a
Alfred79 <34650397+Alfred79@users.noreply.github.com>
2019-04-03 11:34:48 +0200

    påbörjat animationstest

---
+++ src/test/java/animationTests/animationBehaviourTest.java
@@ -0,0 +1,24 @@
+package animationTests;
+
+import org.junit.Test;
+
+import museumHeist.GUI.Classes.GameBoardInterface;
+import museum_heist.Position;
+
+public class animationBehaviourTest {
+//
+//	
+//@Test 
+//void checkThatCharacterSwithcesAppearanceWhenMoving() {
+//
+//	GameBoardInterface gb = new GameBoardInterface();
+//	gb.getCharacter().getCurrentOutfit(); 
+//	gb.getCharacter().setCurrentPosition(1, 1);//spelkaraktären flyttas till en specifik position
+//	gb.getCharacter().switchOutfit(); 
+//	
+//	
+//	
+//}
+	
+	
+}
