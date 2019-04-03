7c37ce9f9e1923fe5e2cbbd16d39542fc853ffb9
Alfred79 <34650397+Alfred79@users.noreply.github.com>
2019-04-03 14:04:21 +0200

    musikloopen löper i egen tråd, hoppas det löser buggen med att musiken bryts ibland

--- sounds/soundTest/SoundPlayer.java
+++ sounds/soundTest/SoundPlayer.java
@@ -4,16 +4,18 @@ import java.applet.Applet;
 import java.applet.AudioClip;
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.GregorianCalendar;
 
 public class SoundPlayer {
 	//Objekt för at spela upp ljud
-	String levelMusic = "198896__bone666138__8-bit-circus-music.wav";
-	String shine = "399904__mrthenoronha__shining-8-bit.wav";
-	String boss = "179684__nicolasdrweski__mario-boss.wav";
-	String enterDoor = "270319__littlerobotsoundfactory__jingle-win-01.wav";
+String levelMusic = "198896__bone666138__8-bit-circus-music.wav";
+String shine = "399904__mrthenoronha__shining-8-bit.wav";
+String boss = "179684__nicolasdrweski__mario-boss.wav";
+String enterDoor = "270319__littlerobotsoundfactory__jingle-win-01.wav";
 String evolve = "391656__jeckkech__evolve.wav";
 String respawn = "458251__tolerabledruid6__you-died.wav";
-	private AudioClip gameSoundclip;
+
+private AudioClip gameSoundclip;
   
 private File levelSoundFile = new File("sounds/resources/Soundfiles/"+levelMusic);
 private File treasureSoundFile = new File("sounds/resources/Soundfiles/"+shine);
@@ -23,22 +25,28 @@ private File respawnSoundFile = new File("sounds/resources/Soundfiles/"+respawn)
 
   
   
-  ///soundTest/src/resources/Soundfiles/265308__volvion__8-bit-bossfight.mp3
-  
+ 
+  //loopar musiken i egen tråd
   public void loopLevelSound() {
 	
-		try {
-			//skapa nytt ljudklipp 
-			gameSoundclip = Applet.newAudioClip(new File(levelSoundFile.toString()).toURI().toURL());
-			gameSoundclip.loop();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		//
-		//alarmSoundclip.loop();
-		//alarmSoundclip.stop();
-		
+	  new Thread() {
+			public void run() {
+				try {
+
+					
+					//skapa nytt ljudklipp 
+					gameSoundclip = Applet.newAudioClip(new File(levelSoundFile.toString()).toURI().toURL());
+					gameSoundclip.loop();
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		}.start();
 	}
+
+		
+	
 	
   public void playTreasureSound() {
 		
